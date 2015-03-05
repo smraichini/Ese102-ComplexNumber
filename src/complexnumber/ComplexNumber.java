6ee397567f97eea054429ec6f6a3b929bb13676b
smraichini <siamon100@gmail.com>
2015-03-05 21:33:56 +0100

    Main and JavaDoc implemented

--- src/complexnumber/ComplexNumber.java
+++ src/complexnumber/ComplexNumber.java
@@ -10,38 +10,92 @@ public class ComplexNumber {
     private double re;
     private double im;
  
+    /**
+  * Stampa il numero immaginario nel formato seguente: Parte Reale+(Parte Immaginaria)
+  * @return 
+  */
     public String formatComplexNumber() {
         String r = this.getRe() + "+(" + this.getIm() + ")i";
         return r;
     }
 
-    public double getRe() {
-        return re;
-    }
-
-    public double getIm() {
-        return im;
-    }
-    
+    /**
+ * Richiede modulo e argomento,considerare che la classe gestisce tutti gli angoli in modo sessagesimale
+ * e che il modulo non può essere minore di 0
+ * @param mod
+ * @param arg 
+ */
     public void setPolar(double mod, double arg){
-        this.im=Math.sin(arg)*mod;
-        this.re=Math.cos(arg)*mod;
+     if(mod>=0)
+     {
+        this.re = Math.cos(arg*Math.PI/180)*mod;
+        if(arg == 180){
+          this.im = 0;
         }
+        else{
+         this.im = Math.sin(arg*Math.PI/180)*mod;
+        }
+     }
+     else
+     {
+        throw new IllegalArgumentException("Il valore del modulo deve essere maggiore di 0");
+     }
+    }
     
-    public void setCartesian(double re, double im ){
+    /**
+     * Richiede le coordinate rettangolari di x e y
+     * @param re
+     * @param im 
+     */
+    public void setRectangular(double re, double im ){
         this.im=im;
         this.re=re;
         }
     
-    
+    /**
+     * Restiutisce il valore del modulo
+     * @return 
+     */
     public double getModulus(){
         
         return(Math.sqrt(Math.pow(this.re,2)+Math.pow(this.im,2)));
     }
     
+    /**
+     * Restituisce il valore dell'argomento in (angoli sessagesimali)
+     * @return 
+     */
     public double getArgument(){
-        return((Math.atan(this.im/this.re)*180)/3.14);
+     int bugFix = 0;
+     if(this.re  < 0 && this.im == 0){
+     bugFix = 180;
+    }
+    return((Math.atan(this.im/this.re)*180)/Math.PI+bugFix);
     }
     
+    /**
+     * Restituisce il valore della parte Reale
+     * @return 
+     */  
+    public double getRe() {
+        return re;
+    }
     
+    /**
+     * Restituisce il valore della parte Immaginaria
+     * @return 
+     */
+    public double getIm() {
+        return im;
+    }
 }
+
+/*
+        Nella classe non sono presenti metodi costruttori perchè essendo questa classe 
+        una classe che gestisce numeri complessi sia attraverso le coordinate Cartesiane che
+        quelle Polari, entrambi i metodi costruttori richiederebbero come parametri due valori 
+        double e ,di conseguenza, non potendo esistere due metodi costruttori che richiedono lo stesso 
+        numero di parametri con lo stesso tipo nello stesso ordine,ho deciso di non preferire nessuno
+        dei duei metodi all'altro, l'utente così dovrà utilizzare semplicemente o il metodo setRectangular
+        o il metodo setPolar per inizializzare lo stato dei campi.
+    */
--- src/ese102/Ese102.java
+++ src/ese102/Ese102.java
@@ -5,6 +5,7 @@
  */
 package ese102;
 import complexnumber.*;
+import java.io.*;
 /**
  *
  * @author Simone
@@ -14,11 +15,108 @@ public class Ese102 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input= new InputStreamReader(System.in);
+        BufferedReader tastiera= new BufferedReader(input);
+        String r;
+        int w=0;
+        boolean v=false;
+        double x = 0,y = 0;
         ComplexNumber n1=new ComplexNumber();
-        n1.setCartesian(-3, 3);
-        System.out.println(n1.getModulus());
-        System.out.println(n1.getArgument());
+        ComplexNumber n2=new ComplexNumber();
+        System.out.println("Benvenuto nel più innovativo software di gestione di coordinate polari e rettangolari");
+        while(w!=1 && w!=2)
+        {
+            try{
+                System.out.println("Scegliere il tipo di coordinate da convertire Cartesiane/Polari (1/2)");
+                r= tastiera.readLine();
+               
+                try {
+                    w=Integer.valueOf(r).intValue();
+                }
+                catch (NumberFormatException e ){
+                    System.out.println("Scelta non valida ");
+                }
+                
+        if(w==1){
+        while(!v)
+        {
+                
+            System.out.print("Inserisci il valore della parte reale: ");
+            r= tastiera.readLine();
+            try {
+                x=Double.valueOf(r).doubleValue();
+                v = true;
+            }
+            catch (NumberFormatException e ){
+                System.out.println("Numero non valido");
+                    
+            }        
+        }
+        v=false;
+        
+        while(!v)
+        {
+                System.out.print("Inserisci il valore della parte immaginaria: ");
+               r= tastiera.readLine();
+                try {
+                    y=Double.valueOf(r).doubleValue();
+                    v = true;
+                }
+                catch (NumberFormatException e ){
+                    System.out.println("Numero non valido");
+                }
+         }
+        n1.setRectangular(x, y);
+        System.out.println("Il valore del modulo è"+" "+n1.getModulus());
+        System.out.println("Il valore dell'argomento è"+" "+n1.getArgument());
+        }
+        else if(w==2){
+            while(!v)
+            {               
+                System.out.print("Inserisci il valore dell'Argomento: ");
+                r= tastiera.readLine();
+                try {
+                    y=Double.valueOf(r).doubleValue();
+                    v = true;
+                }
+                catch (NumberFormatException e ){
+                    System.out.println("Numero non valido");
+                }
+                 
+        }
+        v=false;
+        while(!v)
+        {
+                System.out.print("Inserisci il valore del Modulo: ");
+               r= tastiera.readLine();
+                try {
+                    x=Double.valueOf(r).doubleValue();
+                    n1.setPolar(x, y);
+                    v = true;
+                }
+                catch (NumberFormatException e ){
+                    System.out.println("Numero non valido");
+                }
+           catch(IllegalArgumentException e)
+                {
+                System.out.println("Il valore del modulo deve essere maggiore o uguale a 0");
+                }
+
+                    
+        }
+          
+        System.out.println("Il valore di Re è"+" "+n1.getRe());
+        System.out.println("Il valore di Im è"+" "+n1.getIm());
+        
+        }
+
+            }
+           catch(NumberFormatException e ){
+                 System.out.println("Scelta non valida");
+           }
     }
-    
+      
+    }
+      
 }
